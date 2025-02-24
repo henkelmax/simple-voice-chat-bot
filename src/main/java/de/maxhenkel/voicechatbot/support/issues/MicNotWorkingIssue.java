d32e4462e469058af65f9b809696aaaec0e0df00
Max Henkel <max@maxhenkel.de>
2025-02-24 17:59:26 +0100

    Update microphone not working issue type

--- src/main/java/de/maxhenkel/voicechatbot/support/issues/MicNotWorkingIssue.java
+++ src/main/java/de/maxhenkel/voicechatbot/support/issues/MicNotWorkingIssue.java
@@ -16,7 +16,7 @@ public class MicNotWorkingIssue extends BaseIssue {
     protected List<String> getQuestionsInternal() {
         List<String> questions = super.getQuestionsInternal();
         questions.add("Which operating system are you using? *(Windows/MacOS/Linux)*");
-        questions.add("Which operating system version are you using? (MacOS 10.15/Windows 10 etc)");
+        questions.add("Which operating system version are you using? (MacOS 14/Windows 11 etc)");
         return questions;
     }
 
@@ -25,7 +25,7 @@ public class MicNotWorkingIssue extends BaseIssue {
         textChannel.sendMessageEmbeds(new EmbedBuilder()
                 .setTitle("Disclaimer")
                 .setDescription("""
-                        If you are on **MacOS**, you need to use [Prism Launcher](https://prismlauncher.org/) to get access to your microphone!
+                        If you are on **MacOS**, you need to use [Prism Launcher](https://prismlauncher.org/) or any other supported launcher to get access to your microphone!
                         For more information read [this](https://modrepo.de/minecraft/voicechat/wiki/macos).
                         """)
                 .setColor(Color.RED)
