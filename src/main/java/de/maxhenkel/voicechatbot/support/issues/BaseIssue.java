31c63366c48bca60d7842e57be3e083ffa030ebf
Max Henkel <max@maxhenkel.de>
2025-02-24 17:46:36 +0100

    Change issue question bullet point style

--- src/main/java/de/maxhenkel/voicechatbot/support/issues/BaseIssue.java
+++ src/main/java/de/maxhenkel/voicechatbot/support/issues/BaseIssue.java
@@ -65,7 +65,7 @@ public class BaseIssue implements Issue {
                         %s
                         
                         **Once you answered every question, confirm them by pressing the `Confirm` button.**
-                        """.formatted(getQuestions().stream().map("⦁ %s"::formatted).collect(Collectors.joining("\n"))))
+                        """.formatted(getQuestions().stream().map("- %s"::formatted).collect(Collectors.joining("\n"))))
                 .setColor(Color.BLUE).build()
         ).addComponents(ActionRow.of(
                 SupportThreadUtils.closeThreadButton(),
