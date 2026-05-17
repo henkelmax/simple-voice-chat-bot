7be880625bb9ffb3d308c9d69796efd7420384fe
Max Henkel <max@maxhenkel.de>
2026-05-17 08:46:16 +0200

    Update config issue type

--- src/main/java/de/maxhenkel/voicechatbot/support/issues/ConfigIssue.java
+++ src/main/java/de/maxhenkel/voicechatbot/support/issues/ConfigIssue.java
@@ -19,8 +19,6 @@ public class ConfigIssue extends BaseIssue {
                         While editing configuration files, make sure the client/server is stopped.
                         If the config values keep resetting, this is most likely the problem.
                         If you can't find the config files, make sure the client/server was started at least once, so that the files are generated.
-                        
-                        If you are using an older version of the mod (older than 2.4.0) read [this](https://modrepo.de/minecraft/voicechat/wiki/old_configuration).
                         """)
                 .addField("Fabric/Forge/Quilt config location", "*Server*:\n`config/voicechat/voicechat-server.properties`\n*Client*:\n`config/voicechat/voicechat-client.properties`", false)
                 .addField("Bukkit/Spigot/Paper config location", "`plugins/voicechat/voicechat-server.properties`", false)
