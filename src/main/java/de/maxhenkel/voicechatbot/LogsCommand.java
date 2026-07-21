0c3fc1ad115fd600445cb84fab844a2fa4eddac6
Max Henkel <max@maxhenkel.de>
2026-07-21 12:50:35 +0200

    Update logs command note

--- src/main/java/de/maxhenkel/voicechatbot/LogsCommand.java
+++ src/main/java/de/maxhenkel/voicechatbot/LogsCommand.java
@@ -26,7 +26,7 @@ public class LogsCommand {
                 On the server your logs are located in your Minecraft server directory in `logs/latest.log`.
                 
                 
-                *Note that any logs you send here will be uploaded to [mclo.gs](https://mclo.gs)!*
+                *Note that any logs you send here will be automatically uploaded to [mclo.gs](https://mclo.gs)!*
                 """);
         builder.setUrl("https://modrepo.de/minecraft/how_to_get_logs");
 
